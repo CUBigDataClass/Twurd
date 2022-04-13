30144ea3874c52c9c709417ebc6bd504e0c2a026
Isha Karki <iska6979@colorado.edu>
2022-04-13 15:56:54 -0600

    pushing info to db

--- mongo/mongodbtest.py
+++ mongo/mongodbtest.py
@@ -2,8 +2,12 @@ from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb://ishakarki:Boulder92@cluster0-shard-00-00.qwbs1.mongodb.net:27017,cluster0-shard-00-01.qwbs1.mongodb.net:27017,cluster0-shard-00-02.qwbs1.mongodb.net:27017/twurd?ssl=true&replicaSet=atlas-z12xl7-shard-0&authSource=admin&retryWrites=true&w=majority", connect=False)
-db=client.test
+client = MongoClient("mongodb://ishakarki:Boulder92@cluster0-shard-00-00.qwbs1.mongodb.net:27017,cluster0-shard-00-01.qwbs1.mongodb.net:27017,cluster0-shard-00-02.qwbs1.mongodb.net:27017/sample_airbnb?ssl=true&replicaSet=atlas-z12xl7-shard-0&authSource=admin&retryWrites=true&w=majority")
+db = client["sample_airbnb"]
+collection = db["listingsAndReviews"]
 # Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
+# serverStatusResult=db.command("serverStatus")
+# pprint(serverStatusResult)
+post = {"_id":0, "listing_url":"https://www.airbnb.com/rooms/23982323"}
+
+collection.insert_one(post)

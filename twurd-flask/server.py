8b138d048aa5ab671f1a54050455a49ff178eef6
Vananh Le <vale5181@colorado.edu>
2022-04-21 13:50:48 -0600

    in progress mongo connection

--- twurd-flask/server.py
+++ twurd-flask/server.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify, request
+from flask.ext.pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'DaRealDeal'
+app.config['MONGO_URI'] = 'mongodb://ishakarki:Boulder92@cluster0-shard-00-00.qwbs1.mongodb.net:27017,cluster0-shard-00-01.qwbs1.mongodb.net:27017,cluster0-shard-00-02.qwbs1.mongodb.net:27017/DaRealDeal?ssl=true&replicaSet=atlas-z12xl7-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 #https://www.youtube.com/watch?v=7LNl2JlZKHA
 @app.route("/frequencies", methods=['GET'])
 def get_all_frequencies():
-        return {"state": "Alabama", "words": ["doge", "football", "brother"]}
+        framework = mongo.db.framework
+        output = []
+
+        for q in framework.find():
+                output.append({'state': q.state, 'words': [q.words[0], q.words[1], q.words[2]]})
+        return jsonify({'result': output})
+        
+        # return {"state": "Alabama", "words": ["doge", "football", "brother"]}
     
 @app.route("/frequencies/<state>", methods=['GET'])
 def get_one_frequency(state):

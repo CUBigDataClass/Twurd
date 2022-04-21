80f65267f4d100d6e1e93f0fec0f1fdd17af3afd
Amey Erdenebileg <ameyerdenebileg@engr2-11-64-dhcp.int.colorado.edu>
2022-04-21 17:16:50 -0600

    updated db

--- mongo/teststream.py
+++ mongo/teststream.py
@@ -1,8 +1,9 @@
 # referenced: https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Filtered-Stream/filtered_stream.py
+# https://towardsdatascience.com/an-extensive-guide-to-collecting-tweets-from-twitter-api-v2-for-academic-research-using-python-3-518fcb71df2a
+# https://developer.twitter.com/en/docs/twitter-api/tweets/search/api-reference/get-tweets-search-all#tab1
 
 from telnetlib import TLS
 import requests
-import os
 import json
 import tweepy
 import config
@@ -15,7 +16,7 @@ api = tweepy.API(auth)
 client = MongoClient("mongodb://ishakarki:Boulder92@cluster0-shard-00-00.qwbs1.mongodb.net:27017,cluster0-shard-00-01.qwbs1.mongodb.net:27017,cluster0-shard-00-02.qwbs1.mongodb.net:27017/DaRealDeal?ssl=true&replicaSet=atlas-z12xl7-shard-0&authSource=admin&retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
 db = client.test
 db = client["DaRealDeal"]
-collection = db["raw_tweets"]
+collection = db["us_tweets"]
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
@@ -33,31 +34,25 @@ def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2FilteredStreamPython"
     return r
-#"place_id": "01da545b582fc86b"
+
 def connect_to_endpoint(url, next_token = None):
     query_params = {
-                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
-                    'place.fields': 'full_name,country_code,country',
+                    "expansions": "author_id,in_reply_to_user_id,geo.place_id",
+                    "place.fields": "full_name,country_code,country",
                     }
-    query_params['next_token'] = next_token
+    query_params["next_token"] = next_token
     
     response = requests.request("GET", url, auth=bearer_oauth, stream=True, params=query_params)
     print(response.status_code)
     for response_line in response.iter_lines():
         if response_line != None or response_line != '':
             json_response = json.loads(response_line)
-            # id = str(json_response['data']['author_id'])
-            # user = api.get_user(user_id=id)
-            # location = user.location
-            # print(location)
-            #print(json_response['data']['author_id'])
+            
             if json_response['data']['geo'] and json_response['includes']['places'][0]['country_code'] == "US":
                 print(json.dumps(json_response, indent=4, sort_keys=True))
                 collection.insert_one(json_response)
-                
-            #print(json.dumps(json_response, indent=4, sort_keys=True))
-            # mongo tech
 
+            
     if response.status_code != 200:
         raise Exception(
             "Request returned an error: {} {}".format(

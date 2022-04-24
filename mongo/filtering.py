369fd6b74d4a85df420dfae6ecf51a6267201351
davidgao02 <daga3672@colorado.edu>
2022-04-24 13:56:42 -0600

    updated the filter -David, Isha, Amey

--- mongo/filtering.py
+++ mongo/filtering.py
@@ -14,24 +14,26 @@ api = tweepy.API(auth)
 
 client = MongoClient("mongodb://ishakarki:Boulder92@cluster0-shard-00-00.qwbs1.mongodb.net:27017,cluster0-shard-00-01.qwbs1.mongodb.net:27017,cluster0-shard-00-02.qwbs1.mongodb.net:27017/DaRealDeal?ssl=true&replicaSet=atlas-z12xl7-shard-0&authSource=admin&retryWrites=true&w=majority", tls=True, tlsAllowInvalidCertificates=True)
 db = client["DaRealDeal"]
-# collection = db["us_tweets"]
+collection = db["states"]
 
 bearer_token = config.BEARER_TOKEN
 
-states = [ '.AK', '.AL', '.AR', '.AZ', '.CA', '.CO', '.CT', '.DC', '.DE', '.FL', '.GA',
+states = [ '.AK', '.AL', '.AR', '.AZ', '.CA', '.CO', '.CT', '.DE', '.FL', '.GA',
            '.HI', '.IA', '.ID', '.IL', '.IN', '.KS', '.KY', '.LA', '.MA', '.MD', '.ME',
            '.MI', '.MN', '.MO', '.MS', '.MT', '.NC', '.ND', '.NE', '.NH', '.NJ', '.NM',
            '.NV', '.NY', '.OH', '.OK', '.OR', '.PA', '.RI', '.SC', '.SD', '.TN', '.TX',
            '.UT', '.VA', '.VT', '.WA', '.WI', '.WV', '.WY']
 
-state = [ '.AK' ]
-
+state = ['.DC']
 for name in state:
     megastring = ""
     for doc in db.us_tweets.find({"includes.places.0.full_name": { "$regex": name}}):
         temp = cleaner.clean(doc["data"]["text"])
         megastring += temp
-    print(megastring)
+    megastring = " ".join(megastring.split())
+    post = {"state":name[-2:], "combined_text": megastring}
+    # print(post)
+    collection.insert_one(post)
     
 
 
